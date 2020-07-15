30396f6cfbc2e3a087f7f19478cdfd77863fbd46
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 00:24:42 +0530

    adding part time employee and wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -2,12 +2,23 @@ public class EmployeeWageProblem
   {
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR =  8;
- 
+    public static final int IS_PART_TIME  = 1;
+    public static final int IS_FuLL_TIME = 2;    
+    public static final int PART_TIME_HOUR = 4;
 
-public static void computeDailyWage() {
-    int totalDailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-     System.out.println("totalDailywage is: "+ totalDailyWage);
+public static void computeDailyWage()
+ {
+    int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
+     if (empTypeCheck == 1)
+  {   
+     int part_TimeTotalDailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+     System.out.println("part time totalWage : " + part_TimeTotalDailyWage);
+}
+   else {
+    int full_TimeTotalDailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+     System.out.println("full time totalDailywage is: "+ full_TimeTotalDailyWage);
    }
+}
 public static void main(String[] args)
     {
     
