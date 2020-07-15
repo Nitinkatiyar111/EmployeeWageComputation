b599c6368f386a25e13eeb1b43417a8d73510f7f
nitin katiyar <nkatiyar11@gmail.com>
2020-07-15 23:37:38 +0530

    Calculating Daily EmployeeWage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,6 +1,14 @@
 public class EmployeeWageProblem 
   {
- public static void main(String[] args)
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOUR =  8;
+ 
+
+public static void computeDailyWage() {
+    int totalDailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+     System.out.println("totalDailywage is: "+ totalDailyWage);
+   }
+public static void main(String[] args)
     {
     
       int min =0;
@@ -15,6 +23,7 @@ System.out.println("Welcome to Employee Wage Computation program");
       if(empCheck == 0)
    {
        System.out.println("Employee is present");
+        computeDailyWage();
 
  }
   else
