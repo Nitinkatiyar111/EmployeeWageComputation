c2fbc1d16d60fce96c2f2ca1722d17c5e78fd386
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 18:11:04 +0530

    Added empWage for multiple companies

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,45 +1,59 @@
 public class EmployeeWageProblem 
   {
-    public static final int EMP_RATE_HOUR = 20;
     public static final int IS_PART_TIME  = 1;
-    public static final int IS_FULL_TIME = 2;    
+    public static final int IS_FULL_TIME = 2;
+    
+    private final String company;
+    private final int empRatePerHour;
+    private final int maxWorkingDays;
+    private final int maxWorkingHour;
+    private int totalEmpWage;
+public EmployeeWageProblem(String company,int empRatePerHour,int maxWorkingDays, int maxWorkingHour)
+   { 
+       this.company = company;
+       this.empRatePerHour = empRatePerHour;
+       this.maxWorkingDays = maxWorkingDays;
+       this.maxWorkingHour = maxWorkingHour;      
+}
 
-public static void computeDailyWage() 
- {
-     int totalWage = 0;
-     int empHrs = 0;
-     int workingHour = 0;
-     int workingDay  = 1;
-     int maxWorkingDay  = 20;
-     int  maxWorkingHour = 100;
-   while ( workingDay < maxWorkingDay || workingHour < maxWorkingHour)
+public void computeDailyWage() 
+  {
+       int empHrs = 0;
+       int workingHour = 0;
+       int totalWorkingDay  = 0;
+
+   while ( totalWorkingDay < maxWorkingDays || workingHour < maxWorkingHour)
   
-   {
-      int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
-       switch (empTypeCheck) 
     {
-       case IS_PART_TIME:
-        empHrs = 4 ;
+         totalWorkingDay++;
+         int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
+       
+  switch (empTypeCheck) 
+     {
+          case IS_PART_TIME:
+          empHrs = 4 ;
             break;
-       case IS_FULL_TIME:
-       empHrs = 8;
+         case IS_FULL_TIME:
+         empHrs = 8;
           break;
     default:
          empHrs = 0;
- }   
-    totalWage += empHrs *  EMP_RATE_HOUR  ;
-     workingHour = workingHour + 8;
-     workingDay = workingDay + 1;
-  }
-System.out.println("totalWage is :"+ totalWage);   
+      } 
+    workingHour += empHrs;
+  
+    } 
+    totalEmpWage += empHrs * empRatePerHour ; 
+ }
+    public String toString() {
+     return "Total emp wage for company: " +company+ " is: "+ totalEmpWage;
 }
-public static void main(String[] args)
-    {
-    
-      int min =0;
-      int max =1; 
-      int isPresent = 0;
-      int isAbsent  = 1;
+ public void  checkAttendance() 
+     {
+           
+           int min =0;
+           int max =1; 
+           int isPresent = 0;
+           int isAbsent  = 1;
 
 System.out.println("Welcome to Employee Wage Computation program");
 
@@ -51,10 +65,22 @@ System.out.println("Welcome to Employee Wage Computation program");
                   computeDailyWage();
                       
 
- }
+    }
   else
-     System.out.println("Employee is absent");
-   
+      System.out.println("Employee is absent");
+}   
+
+public static void main(String[] args)
+      {
+    
+    EmployeeWageProblem dMart = new EmployeeWageProblem("DMart",30,2,10);
+       dMart.checkAttendance();
+       dMart.computeDailyWage();
+       System.out.println(dMart);
+    EmployeeWageProblem reliance = new EmployeeWageProblem("Reliance",40,4,10);
+        reliance.checkAttendance();
+        reliance.computeDailyWage();
+       System.out.println(reliance); 
 
     }
 
