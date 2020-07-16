32c2d3953b3d15ab4d069cced88862d70d492a83
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 11:21:25 +0530

    calculating employeeWage for 20 day

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -3,23 +3,24 @@ public class EmployeeWageProblem
     public static final int WAGE_PER_HOUR = 20;
     public static final int IS_PART_TIME  = 1;
     public static final int IS_FULL_TIME = 2;    
-
 public static void computeDailyWage()
-  {
+  {  int totalWorkingDay = 20;
      int empHrs = 0;
+     int totalEmpHrs = 0;
      int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
      switch (empTypeCheck) 
  {
      case IS_PART_TIME:
-       empHrs = 4;
+       empHrs = 4 * totalWorkingDay;
           break;
     case IS_FULL_TIME:
-       empHrs = 8;
+       empHrs = 8 * totalWorkingDay;
           break;
     default:
          empHrs = 0;
  }
-int totalWage = empHrs * WAGE_PER_HOUR ;
+totalEmpHrs +=empHrs ;   
+int totalWage = totalEmpHrs * WAGE_PER_HOUR ;
 System.out.println("totalWage is :"+ totalWage);
 }
 public static void main(String[] args)
