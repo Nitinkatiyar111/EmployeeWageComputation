457be7bf3263942dc74a399fb5590ff479dd4c20
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 12:09:58 +0530

    conditionally workingHour & day employeeWage calculated

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -4,24 +4,35 @@ public class EmployeeWageProblem
     public static final int IS_PART_TIME  = 1;
     public static final int IS_FULL_TIME = 2;    
 public static void computeDailyWage()
-  {  int totalWorkingDay = 20;
+  { 
+     int totalWage = 0;
+     int totalWorkingDay = 20;
      int empHrs = 0;
      int totalEmpHrs = 0;
-     int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
-     switch (empTypeCheck) 
- {
-     case IS_PART_TIME:
-       empHrs = 4 * totalWorkingDay;
-          break;
-    case IS_FULL_TIME:
+     int workingHour = 0;
+     int workingDay  = 1;
+     int maxWorkingDay  = 20;
+     int  maxWorkingHour = 100;
+while ( workingHour < maxWorkingDay || workingHour < maxWorkingHour)
+  {
+      int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
+       switch (empTypeCheck) 
+    {
+       case IS_PART_TIME:
+        empHrs = 4 * totalWorkingDay;
+            break;
+       case IS_FULL_TIME:
        empHrs = 8 * totalWorkingDay;
           break;
     default:
          empHrs = 0;
  }
 totalEmpHrs +=empHrs ;   
-int totalWage = totalEmpHrs * WAGE_PER_HOUR ;
-System.out.println("totalWage is :"+ totalWage);
+    totalWage = totalEmpHrs * WAGE_PER_HOUR ;
+     workingHour = workingHour + 8;
+     workingDay = workingDay + 1;
+  }
+System.out.println("totalWage is :"+ totalWage);   
 }
 public static void main(String[] args)
     {
