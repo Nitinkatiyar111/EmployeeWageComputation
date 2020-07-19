07347c4c07650597c100a536bef72a11b947b9a4
nitin katiyar <nkatiyar11@gmail.com>
2020-07-19 16:55:32 +0530

    indentation corrected

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,83 +1,83 @@
 public class EmployeeWageProblem 
   {
-    public static final int IS_PART_TIME  = 1;
-    public static final int IS_FULL_TIME = 2;
+        public static final int IS_PART_TIME  = 1;
+        public static final int IS_FULL_TIME = 2;
     
-    private final String company;
-    private final int empRatePerHour;
-    private final int maxWorkingDays;
-    private final int maxWorkingHour;
-    private int totalEmpWage;
-public EmployeeWageProblem(String company,int empRatePerHour,int maxWorkingDays, int maxWorkingHour)
-   { 
+        private final String company;
+        private final int empRatePerHour;
+        private final int maxWorkingDays;
+        private final int maxWorkingHour;
+        private int totalEmpWage;
+
+
+      public EmployeeWageProblem(String company,int empRatePerHour,int maxWorkingDays, int maxWorkingHour)
+    { 
        this.company = company;
        this.empRatePerHour = empRatePerHour;
        this.maxWorkingDays = maxWorkingDays;
        this.maxWorkingHour = maxWorkingHour;      
-}
+  }
 
-public void computeDailyWage() 
+    public void computeDailyWage() 
   {
        int empHrs = 0;
        int workingHour = 0;
        int totalWorkingDay  = 0;
 
-   while ( totalWorkingDay < maxWorkingDays || workingHour < maxWorkingHour)
-  
+      while ( totalWorkingDay < maxWorkingDays || workingHour < maxWorkingHour)
     {
          totalWorkingDay++;
          int empTypeCheck  = (int) Math.floor(Math.random() * 10) % 3;
        
-  switch (empTypeCheck) 
-     {
-          case IS_PART_TIME:
-          empHrs = 4 ;
-            break;
-         case IS_FULL_TIME:
-         empHrs = 8;
-          break;
+       switch (empTypeCheck) 
+        {
+              case IS_PART_TIME:
+              empHrs = 4 ;
+              break;
+              case IS_FULL_TIME:
+              empHrs = 8;
+              break;
     default:
          empHrs = 0;
-      } 
-    workingHour += empHrs;
-  
-    } 
-    totalEmpWage += empHrs * empRatePerHour ; 
- }
-    public String toString() {
-     return "Total emp wage for company: " +company+ " is: "+ totalEmpWage;
+         } 
+         workingHour += empHrs;
+     
+        } 
+        totalEmpWage += empHrs * empRatePerHour ; 
+   }
+       public String toString() {
+       return "Total emp wage for company: " +company+ " is: "+ totalEmpWage;
 }
- public void  checkAttendance() 
-     {
+     public void  checkAttendance() 
+  {
            
            int min =0;
            int max =1; 
            int isPresent = 0;
            int isAbsent  = 1;
 
-System.out.println("Welcome to Employee Wage Computation program");
+     System.out.println("Welcome to Employee Wage Computation program");  
+     int empCheck = (int)(Math.random() * (max - min + 1) + min);
 
-   int empCheck = (int)(Math.random() * (max - min + 1) + min);
-
-      if(empCheck == 0)
-   {
-       System.out.println("Employee is present");
+    if(empCheck == 0)
+  {
+                  System.out.println("Employee is present");
                   computeDailyWage();
                       
 
-    }
-  else
-      System.out.println("Employee is absent");
+ }
+     else
+        System.out.println("Employee is absent");
 }   
 
-public static void main(String[] args)
-      {
+    public static void main(String[] args)
+  {
     
     EmployeeWageProblem dMart = new EmployeeWageProblem("DMart",30,2,10);
        dMart.checkAttendance();
        dMart.computeDailyWage();
        System.out.println(dMart);
-    EmployeeWageProblem reliance = new EmployeeWageProblem("Reliance",40,4,10);
+       EmployeeWageProblem reliance = new EmployeeWageProblem("Reliance",40,4,10);
         reliance.checkAttendance();
         reliance.computeDailyWage();
        System.out.println(reliance); 
